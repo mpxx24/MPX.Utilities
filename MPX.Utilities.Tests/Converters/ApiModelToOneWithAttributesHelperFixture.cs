92ff2dd0c3e3e01d307bfb84bc528b03f4e17680
Mariusz Piątkowski <mpxx24@gmail.com>
2018-04-19 19:56:04 +0200

    ApiModelToOneWIthAttributesHelper + tests

---
+++ MPX.Utilities.Tests/Converters/ApiModelToOneWithAttributesHelperFixture.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using MPX.Utilities.Converters;
+using NUnit.Framework;
+
+// ReSharper disable UnusedMember.Local
+// ReSharper disable InconsistentNaming
+namespace MPX.Utilities.Tests.Converters
+{
+    [TestFixture]
+    public class ApiModelToOneWithAttributesHelperFixture {
+        [SetUp]
+        public void SetUp() {
+        }
+
+        [Test]
+        public void ConvertType_Works() {
+            var result = ApiModelToOneWithAttributesHelper.ConvertType(typeof(TestType));
+
+            foreach (var property in typeof(TestType).GetProperties()) {
+                Assert.That(result.Contains($"[JsonProperty(\"{property.Name}\")]"));
+                Assert.That(result.Contains($"{this.GetProperPropertyName(property.Name)}"));
+            }
+
+            //NOTE: just paste it manualy instead of copying methods from implementation
+            Assert.That(result.Contains("double"));
+            Assert.That(result.Contains("List<double>"));
+            Assert.That(result.Contains("List<int>"));
+            Assert.That(result.Contains("List<string>"));
+            Assert.That(result.Contains("double?"));
+            Assert.That(result.Contains("DateTime"));
+            Assert.That(result.Contains("DateTime?"));
+            Assert.That(result.Contains("TestType2"));
+            Assert.That(result.Contains("List<TestType2>"));
+        }
+
+        private string GetProperPropertyName(string propName) {
+            var nameParts = propName.Split('_');
+            var sb = new StringBuilder();
+            foreach (var part in nameParts) {
+                sb.Append(CultureInfo.CurrentCulture.TextInfo.ToTitleCase(part));
+            }
+            return sb.ToString();
+        }
+
+        private class TestType {
+            public double somedoublevalue { get; set; }
+            public double another_double_value { get; set; }
+            public List<double> list_of_double_elements { get; set; }
+            public List<int> list_of_int_elements { get; set; }
+            public List<string> list_of_string_elements { get; set; }
+            public List<double> timbre { get; set; }
+            public double? nullable_double { get; set; }
+            public DateTime some_datetime { get; set; }
+            public DateTime? nullable_datetime { get; set; }
+            public TestType2 testtype2 { get; set; }
+            public List<TestType2> List_of_testtype2 { get; set; }
+
+        }
+
+        // ReSharper disable once ClassNeverInstantiated.Local
+        private class TestType2 {
+            public Guid id { get; set; }
+        }
+    }
+}
---
+++ MPX.Utilities/Converters/ApiModelToOneWithAttributesHelper.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MPX.Utilities.Converters {
+    public static class ApiModelToOneWithAttributesHelper {
+        private static readonly Dictionary<Type, string> typeAliases =
+            new Dictionary<Type, string> {
+                {typeof(byte), "byte"},
+                {typeof(sbyte), "sbyte"},
+                {typeof(short), "short"},
+                {typeof(ushort), "ushort"},
+                {typeof(int), "int"},
+                {typeof(uint), "uint"},
+                {typeof(long), "long"},
+                {typeof(ulong), "ulong"},
+                {typeof(float), "float"},
+                {typeof(double), "double"},
+                {typeof(decimal), "decimal"},
+                {typeof(object), "object"},
+                {typeof(bool), "bool"},
+                {typeof(char), "char"},
+                {typeof(string), "string"},
+                {typeof(void), "void"},
+                {typeof(DateTime), "DateTime"}
+            };
+
+        public static string ConvertType(Type modelType) {
+            //do not need anything more than class now
+            var builder = new ApiModelToOneWithAttributesBuilder("class", modelType.Name);
+
+            foreach (var propertyInfo in modelType.GetProperties()) {
+                builder.AddProperty(IsSystemType(propertyInfo.PropertyType) && !IsPropertyOfNullableType(propertyInfo.PropertyType)
+                                        ? GetTypeAlias(propertyInfo.PropertyType)
+                                        : GetGenericPropertyName(propertyInfo.PropertyType), propertyInfo.Name);
+            }
+
+            return builder.GetObjectAsString();
+        }
+
+        public static IEnumerable<string> ConvertMultipleTypes(IEnumerable<Type> types) {
+            return types.Select(ConvertType).ToList();
+        }
+
+        private static bool IsSystemType(Type type) {
+            //hacky hackery hack
+            return type.Namespace == "System";
+        }
+
+        private static string GetTypeAlias(Type propertyType) {
+            //TODO: NOTE CAN NOT USE THIS HERE IN THE NUGET PACKAGE, WORKAROUND NEEDED
+            //using (var codeProvider = new CSharpCodeProvider()) {
+            //    var typeRef = new CodeTypeReference(propertyType);
+            //    return codeProvider.GetTypeOutput(typeRef);
+            //}
+
+            return typeAliases.ContainsKey(propertyType)
+                ? typeAliases[propertyType]
+                : string.Empty;
+        }
+
+        private static string GetGenericPropertyName(Type propertyType) {
+            //non-generics
+            if (!propertyType.IsGenericType) {
+                return propertyType.Name;
+            }
+
+            var sb = new StringBuilder();
+
+            //nullable
+            if (IsPropertyOfNullableType(propertyType)) {
+                if (propertyType.GetGenericArguments().Length == 1) {
+                    sb.Append($"{GetTypeAlias(propertyType.GetGenericArguments()[0])}?");
+                }
+            }
+            else {
+                sb.Append(propertyType.Name);
+
+                if (sb.Length > 0) {
+                    sb.Length -= 2;
+                }
+                //other generics i.e. collections
+                sb.Append("<");
+                foreach (var genericArgument in propertyType.GetGenericArguments()) {
+                    sb.Append(IsSystemType(genericArgument)
+                                  ? $"{GetTypeAlias(genericArgument)}, "
+                                  : $"{genericArgument.Name}, ");
+                }
+
+                if (sb.Length > 0) {
+                    sb.Length -= 2;
+                }
+
+                sb.Append(">");
+            }
+            return sb.ToString();
+        }
+
+        private static bool IsPropertyOfNullableType(Type propertyType) {
+            return Nullable.GetUnderlyingType(propertyType) != null;
+        }
+    }
+}
